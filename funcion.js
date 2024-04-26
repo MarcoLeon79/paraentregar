be356b4ae380f093bd0873a0ac31c6f29529a546
Marco Dario León <mdleon@cervantes.edu.ar>
2024-04-26 20:57:48 -0300

    paraentregar

---
+++ funcion.js
@@ -0,0 +1,4 @@
+// Ajusta la velocidad de transición del carrusel a 1 segundo (1000 milisegundos)
+$('.carousel').carousel({
+    interval: 500 // Aquí puedes ajustar la velocidad según tus preferencias
+});
